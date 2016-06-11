6f51ddb22c4adeb7acba7a8c6e59ddaa6de62757
Mandy Jor <mandy1jor@gmail.com>
2016-06-11 02:51:59 -0500

    FizzBuzz finished.

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/FizzBuzz.java
@@ -28,10 +28,23 @@ package com.thoughtworks.tw101.introductory_programming_exercises;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-        fizzBuzz();
+        fizzBuzz(1,100);
     }
 
-    private static void fizzBuzz() {
+    private static void fizzBuzz(int start, int end) {
+        for(int i = start; i<=end; i++){
+
+            if(i%3==0 && i%5==0){
+                System.out.println("FizzBuzz");
+            }else if(i%3==0 ){
+                System.out.println("Fizz");
+            }else if(i%5==0 ){
+                System.out.println("Buzz");
+            }else{
+                System.out.println(i);
+            }
+
+        }
 
     }
 }
